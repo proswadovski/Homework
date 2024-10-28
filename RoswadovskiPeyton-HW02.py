bcdac1b28752a9ecb07eae85b0843cd50b7cbb8c
proswadovski <proswado@uwyo.edu>
2024-10-28 11:21:18 -0600

    Create RoswadovskiPeyton-HW02.py

---
+++ RoswadovskiPeyton-HW02.py
@@ -0,0 +1,54 @@
+# Peyton Roswadovski
+# UWYO COSC 1010
+# 10/29/24
+# HW 02
+# Lab Section: 14
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+#For this homework assignment you will be writing a program that translates between plaintext and Morse Code.
+
+
+#When your program first starts it should ask the user for the input string. If plaintext alphabet cahracters is entered output the Morse Code equivalent.
+
+#You may assume that only alphabet characters will be entered, and may ignore other input characters.
+
+#You can use the equivalencies below.
+
+#A: .-          N: -.
+#B: -...        O: ---
+#C: -.-.        P: .--.
+#D: -..         Q: --.-
+#E: .           R: .-.
+#F: ..-.        S: ...
+#G: --.         T: -
+#H: ....        U: ..-
+#I: ..          V: ...-
+#J: .---        W: .--
+#K: -.-         X: -..-
+#L: .-..        Y: -.-- 
+#M: --          Z: --..
+
+ 
+
+#Your program should output the correct Morse Code regardless of casing of the input characters. You should output spaces in the input string as two spaces, and separation between  Morse Code characters should be a single space.
+
+#For example the message 'Go Pokes' would be equivalent to:
+
+#--. ---  .--. --- -.- . ...
+
+#Where there is a space between the "G" and "o" in Morse code and two spaces between the "Go" and "Pokes".
+
+#Tips and tricks:
+
+#Dictionaries will be your friend for this assignment
+#You can utilize the `isalpha()` method on a string to see if it is  an alphabet character
+#`if string_variable.isalpha():`
+#The Morse Code characters will only ever be `-` or `.`  or a space
+#You can treat strings much like you would a list, meaning you can iterate through them and access characters based on an index position
+#Remember you can utilize string concatenation with `+=` to build new strings
+#Name your file: LastnameFirstname-HW02.py
+
+#Your file MUST include the standard comments at the top of the file that are expected with the labs.
